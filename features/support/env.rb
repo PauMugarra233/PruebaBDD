f64d3c9bf9d4851068959facb4a8b909af28025a
PauMugarra233 <pau.mugarra@233gradosdeti.com>
2016-04-25 19:28:58 +0200

    Update env.rb
    Añado el modo headless al watir webdriver

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,14 @@
 require 'watir-webdriver'
 require 'minitest/unit'
+require 'headless'
+headless = Headless.new
+headless.start
+browser = Watir::Browser.new :chrome
+browser.goto 'http://google.com'
 
 BASE_URL = "http://moduscreate.com"
 
 World(MiniTest::Assertions)
+
+browser.close
+headless.destroy
